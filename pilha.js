ab21bec50e822e804aeae3605aeacd8b76a752d9
Malu <62160705+malufell@users.noreply.github.com>
2021-08-05 18:42:08 -0300

    Create pilha.js

---
+++ pilha.js
@@ -0,0 +1,26 @@
+function verificaFechamentoParenteses(texto) {
+  let pilha = [];
+  
+  for(let i = 0; i < texto.length; i++) {
+    let caractereInformado = texto[i];
+
+    if(caractereInformado == "(") {
+      pilha.push(caractereInformado);
+      console.log(JSON.stringify(pilha))
+    } 
+    
+    if(caractereInformado == ")") {
+      const topo = pilha.pop();
+      if(topo !== "(") {
+        pilha.push(caractereInformado);
+      }
+      console.log(JSON.stringify(pilha))
+    }
+  }
+
+  if(!texto || pilha.length > 0) return console.log("erro")
+  if(pilha.length == 0) return console.log("OK")
+
+}
+
+verificaFechamentoParenteses("())");
