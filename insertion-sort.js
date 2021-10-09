d93d497bf898e195fce3d8f3c84368fd12e39210
Malu <62160705+malufell@users.noreply.github.com>
2021-10-09 18:44:20 -0300

    Update insertion-sort.js

--- insertion-sort.js
+++ insertion-sort.js
@@ -1,18 +1,18 @@
-function insertionSort(vetor) {
-  for (let i = 1; i < vetor.length; i++) {
-
-    let elem = vetor[i];
-    let elemEsquerda = i - 1;
-
-    while (elemEsquerda >= 0 && vetor[elemEsquerda] > elem) {
-      vetor[elemEsquerda + 1] = vetor[elemEsquerda];
-      elemEsquerda--;
-    }
-
-    vetor[elemEsquerda + 1] = elem;
-  };
-  return vetor;
+function insertionSort(list) {
+    for (let i = 1; i < list.length; i++) {
+  
+      let elem = list[i];
+      let indexLeft = i - 1;
+  
+      while (indexLeft >= 0 && list[indexLeft] > elem) {
+        list[indexLeft + 1] = list[indexLeft];
+        indexLeft--;
+      }
+  
+      list[indexLeft + 1] = elem;
+    };
+    return list;
 };
 
-const vetor = [3, 5, 4, 8, 10, 1];
-console.log(insertionSort(vetor));
+const list = [3, 5, 4, 8, 10, 1];
+console.log(insertionSort(list));
